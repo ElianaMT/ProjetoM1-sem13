a5bd52d90f55e4820ce0ac7fe8b6afb3133549c8
ElianaMT <eliana_taco@estudante.sesisenai.org.br>
2023-08-31 13:49:30 -0500

    Instalando bibliotecas
    •	Vuetify
    •	Yup
    •	Vee-validate
    •	Axios

--- proyecto-m1/src/main.js
+++ proyecto-m1/src/main.js
@@ -2,11 +2,25 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './routes/routes'
 
-
 const app = createApp(App)
 
+// Vuetify
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+      defaultSet: 'mdi'
+    },
+  })
 
 
 app
 .use(router)
+.use(vuetify)
 .mount('#app')
