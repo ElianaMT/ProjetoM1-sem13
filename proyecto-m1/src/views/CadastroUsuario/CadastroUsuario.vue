4418b5b370eb09480bf8eb8dba95ac4ec21724a1
ElianaMT <eliana_taco@estudante.sesisenai.org.br>
2023-09-01 01:35:28 -0500

    feat: "criação formulário Cadastro"

--- proyecto-m1/src/views/CadastroUsuario/CadastroUsuario.vue
+++ proyecto-m1/src/views/CadastroUsuario/CadastroUsuario.vue
@@ -1,3 +1,73 @@
 <template>
-   Cadastro Usuario
+           <v-card
+       class="mx-auto pa-12 pb-8"
+       elevation="8"
+       max-width="448"
+       rounded="lg"
+     >
+     <div class="title">
+      <h1>Crie sua conta</h1>
+   </div>
+     
+
+         <form>
+            <v-text-field
+            type="text"
+            label="Nome Completo"
+            ></v-text-field>
+
+            <v-text-field
+            type="text"
+            label="E-mail"
+            ></v-text-field>
+
+            <v-text-field
+            type="password"
+            label="Senha"
+            ></v-text-field>
+
+            <v-text-field
+            type="password"
+            label=" Confirme a Senha"
+            ></v-text-field>
+
+            <v-select
+            label="Tipo de plano"
+            ></v-select>
+
+      <div class="d-flex flex-column">
+
+           <v-btn
+           color="orange"
+           class="mt-4"
+           block
+           type="submit"
+         >
+           Cadastrar
+         </v-btn>
+
+         <v-btn
+           color="orange"
+           class="mt-4"
+           block
+           >
+           Voltar
+         </v-btn>     
+    </div>
+
+
+         </form>     
+           </v-card>
+
 </template>
+
+<script>
+</script>
+
+<style>
+.title{ 
+   padding-bottom: 25px;
+   color:orange
+  
+}
+</style>
