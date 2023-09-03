3dd0cb247ebd85e0ab123563134da838f7e669d2
ElianaMT <eliana_taco@estudante.sesisenai.org.br>
2023-09-03 03:18:14 -0500

    feat: "Criação cabeçalho"

--- proyecto-m1/src/views/VisualizacaoTreinos/VisualizacaoTreinos.vue
+++ proyecto-m1/src/views/VisualizacaoTreinos/VisualizacaoTreinos.vue
@@ -1,3 +1,40 @@
 <template>
-  Visualizacao Treinos
+  <v-container>
+    <v-row no-gutters class="flex-wrap encabezado">
+      <v-col cols="1" class="flex-grow-0 flex-shrink-0">
+        <v-sheet class="ma-2 pa-4">
+          <v-icon color="orange">mdi-cart</v-icon>
+        </v-sheet>
+      </v-col>
+
+      <v-col cols="11" class="flex-grow-0 flex-shrink-0">
+        <v-sheet class="ma-2 pa-4">
+          Treinos- nome aluno
+        </v-sheet>
+      </v-col>
+    </v-row>
+    <br>
+
+
+    <v-card class="mx-auto pa-12 pb-8" elevation="8" max-width="1200" rounded="lg">
+
+      <v-form>
+
+        formulario
+
+      </v-form>
+    </v-card>
+
+
+  </v-container>
 </template>
+
+<script>
+</script>
+
+<style>
+.encabezado {
+  border-bottom: solid;
+  border-color: orange;
+}
+</style>
