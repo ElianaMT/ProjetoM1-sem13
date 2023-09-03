5de6ecb9df7b9bc23f82b47c4389a2205a7c778b
ElianaMT <eliana_taco@estudante.sesisenai.org.br>
2023-09-02 23:02:09 -0500

    feat: "Criação formulário novo exercício"

--- proyecto-m1/src/views/Dashboard/dashboard.vue
+++ proyecto-m1/src/views/Dashboard/dashboard.vue
@@ -1,3 +1,3 @@
 <template>
- 
+ oi
 </template>
--- proyecto-m1/src/views/GerenciamentoExercicios/GerenciamentoExercicios.vue
+++ proyecto-m1/src/views/GerenciamentoExercicios/GerenciamentoExercicios.vue
@@ -1,3 +1,56 @@
 <template>
+
+     <v-form v-model="valid">
+    <v-container>
+    <div class="title">
+      <h1>Exercicios</h1>
+    </div>
+
+      <v-row>
+        <v-col
+          cols="12"
+          md="10"
+        >
+          <v-text-field
+            
+            label="Digite o nome do exercício"
+            required
+            hide-details
+          ></v-text-field>
+        </v-col>
+
+        <v-col
+          cols="12"
+          md="2"
+        >
+        <v-btn
+           color="orange"
+           class="mt-2"
+           block
+           type="submit"
+           
+         >
+           Cadastrar
+         </v-btn>
+
+        </v-col>
+       </v-row>
+     </v-container>
+    </v-form>   
+
    
-</template>
+ 
+
+  </template>
+
+
+ 
+  <script>
+</script>
+
+<style>
+.title{ 
+   padding-bottom: 25px;
+   color:orange;  
+}
+</style>
