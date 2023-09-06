58110aa4b9de04b70eb80aec69b2c253e70595c2
ElianaMT <eliana_taco@estudante.sesisenai.org.br>
2023-09-06 13:58:51 -0500

    test:"vee-validate desde 0"

--- proyecto-m1/src/views/GerenciamentoExercicios/GerenciamentoExercicios.vue
+++ proyecto-m1/src/views/GerenciamentoExercicios/GerenciamentoExercicios.vue
@@ -1,42 +1,8 @@
 <template>
-    <v-form @submit.prevent="handleSubmit" >
-        <v-text-field placeholder="Cadastre novo exercício" v-model="exercises"></v-text-field>
+    <v-form >
+        <v-text-field placeholder="Cadastre novo exercício" ></v-text-field>
+       
         <v-btn type="submit">Cadastrar</v-btn>
     </v-form>
 </template>
 
-<script>
-import *as yup from "yup"
-
-export default{ 
-    data(){ 
-        return{ 
-            exercises:"",
-            
-        }
-
-    },
-    methods:{ 
-        async handleSubmit(){
-
-            try {
-                const schema= yup.object().shape({ 
-                exercises: yup.string().required("O cadastro do exercício é obrigatorio")
-            })
-            schema.validateSync({ 
-                exercises: this.exercises
-               
-            }, {abortEarly:false })
-                
-            } catch (error) {
-                if(error instanceof yup.ValidationError){
-                    alert(error)
-                }
-                
-            }           
-           
-        }
-    }
-}
-
-</script>
