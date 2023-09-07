53cadad418111709d098a8c38e22658d5b0eb5f3
ElianaMT <eliana_taco@estudante.sesisenai.org.br>
2023-09-06 20:22:22 -0500

    feat: "montando formulario"

--- proyecto-m1/src/views/GerenciamentoExercicios/GerenciamentoExercicios.vue
+++ proyecto-m1/src/views/GerenciamentoExercicios/GerenciamentoExercicios.vue
@@ -1,10 +1,52 @@
 <template>
     <v-form ref="form" @submit.prevent="handleSubmit"  >
-        <v-text-field placeholder="Cadastre novo exercício" v-model="exercicio.exercises"
-        :rules="[value => !!value || 'O exercício é obrigatorio']"></v-text-field>
-       
-        <v-btn type="submit">Cadastrar</v-btn>
+        <v-container>
+
+<v-row no-gutters class="flex-wrap bg-surface-variant encabezado">
+    <v-col cols="1" class="flex-grow-0 flex-shrink-0">
+        <v-sheet class="ma-2 pa-4">
+            <v-icon color="orange" size="x-large">mdi-arm-flex</v-icon>
+
+        </v-sheet>
+    </v-col>
+
+    <v-col cols="11" class="flex-grow-0 flex-shrink-0">
+        <v-sheet class="ma-2 pa-4">
+            Exercícios
+        </v-sheet>
+    </v-col>
+</v-row>
+
+<v-row>
+    <v-col cols="12" md="10">
+        <v-text-field type="text" label="Digite o nome do exercício" v-model="exercicio.exercises"
+            :rules="[value => !!value || 'O cadastro do exercício é obrigatorio']"></v-text-field>
+    </v-col>
+
+    <v-col cols="12" md="2">
+        <v-btn color="orange" class="mt-2" block type="submit">
+            Cadastrar
+        </v-btn>
+
+    </v-col>
+</v-row>
+</v-container>
+
     </v-form>
+
+    <v-table>
+        <thead>
+            <tr>
+                <th>Nome</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>nome producto</td>
+            </tr>
+        </tbody>
+    </v-table>
+
 </template>
 
 <script>
