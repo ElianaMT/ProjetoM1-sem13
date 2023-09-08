48d03d1e780e237e22b802161ff60dd0be43fafb
ElianaMT <eliana_taco@estudante.sesisenai.org.br>
2023-09-08 13:08:25 -0500

    "feat: configuração tabla"
    Para a mostra de informações de treino

--- proyecto-m1/src/views/VisualizacaoTreinos/VisualizacaoTreinos.vue
+++ proyecto-m1/src/views/VisualizacaoTreinos/VisualizacaoTreinos.vue
@@ -9,7 +9,8 @@
 
       <v-col cols="11" class="flex-grow-0 flex-shrink-0">
         <v-sheet class="ma-2 pa-4">
-          Treinos- nome aluno
+          Treinos- 
+           
         </v-sheet>
       </v-col>
     </v-row>
@@ -20,7 +21,62 @@
 
       <v-form>
 
-        formulario
+        <v-table>
+        <thead>
+            <tr>
+                <th>
+                  <v-container>
+                        <v-row>
+                            <v-col cols="12" md="2">
+                                <v-btn color="orange" class="mt-auto" block type="submit" >
+                                  Segunda
+                                </v-btn>
+                            </v-col>
+
+                            <v-col cols="12" md="1">
+                                <v-btn color="orange" class="mt-auto" block type="submit" >
+                                  Terça
+                                </v-btn>
+                            </v-col>
+                            <v-col cols="12" md="1">
+                                <v-btn color="orange" class="mt-auto" block type="submit" >
+                                  Quarta
+                                </v-btn>
+                            </v-col>
+
+                            <v-col cols="12" md="2">
+                                <v-btn color="orange" class="mt-auto" block type="submit" >
+                                  Quinta
+                                </v-btn>
+                            </v-col>
+                            <v-col cols="12" md="2">
+                                <v-btn color="orange" class="mt-auto" block type="submit" >
+                                  Sexta
+                                </v-btn>
+                            </v-col>
+                            <v-col cols="12" md="2">
+                                <v-btn color="orange" class="mt-auto" block type="submit" >
+                                  Sábado
+                                </v-btn>
+                            </v-col>
+                            <v-col cols="12" md="2">
+                                <v-btn color="orange" class="mt-auto" block type="submit" >
+                                  Domingo
+                                </v-btn>
+                            </v-col>
+                        </v-row>
+                    </v-container>
+                </th>                                
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr >
+                <td>lista lorem20</td>  
+            </tr>
+
+        </tbody>
+    </v-table>
 
       </v-form>
     </v-card>
@@ -30,6 +86,28 @@
 </template>
 
 <script>
+import axios from "axios"
+
+export default{ 
+  data() {
+    return {
+      treino:{
+        workout_id: "",
+        student_id:"",
+        day_of_week:"",
+      },
+      usuariosLista : []
+    }
+  },
+
+  
+
+    
+
+  
+
+}
+  
 </script>
 
 <style>
