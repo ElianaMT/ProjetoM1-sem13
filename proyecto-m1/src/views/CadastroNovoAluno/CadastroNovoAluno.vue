108acd4b97eef4d88d18ce9ab3f46c8bbb2ad8ad
ElianaMT <eliana_taco@estudante.sesisenai.org.br>
2023-09-12 19:22:33 -0500

    "feat:  dados de endereço, pré-preenchidos api-CEP"

--- proyecto-m1/src/views/CadastroNovoAluno/CadastroNovoAluno.vue
+++ proyecto-m1/src/views/CadastroNovoAluno/CadastroNovoAluno.vue
@@ -1,6 +1,5 @@
 
 <template>
-
     <v-container>
         <v-row no-gutters class="flex-wrap encabezado">
             <v-col cols="1" class="flex-grow-0 flex-shrink-0">
@@ -21,7 +20,6 @@
         <v-card class="mx-auto pa-12 pb-8" elevation="8" max-width="1200" rounded="lg">
 
             <v-form ref="form" @submit.prevent="handleSubmit">
-                {{ novoAlunoInfo.cep }}
 
                 <v-row>
                     <v-col cols="12" md="6">
@@ -29,15 +27,16 @@
                         type="text" 
                         label="Nome completo" 
                         v-model="alunoNovo.name"
-                        :rules="[value => !!value || 'O nome completo é obrigatorio']"></v-text-field>
-
+                        :rules="[value => !!value || 'O nome completo é obrigatorio']">
+                    </v-text-field>
                     </v-col>
 
                     <v-col cols="12" md="6">
                         <v-text-field 
                         type="email" 
                         label="Email" 
-                        v-model="alunoNovo.email"></v-text-field>
+                        v-model="alunoNovo.email">
+                    </v-text-field>
                     </v-col>
                 </v-row>
 
@@ -47,14 +46,16 @@
                         type="text" 
                         label="Contato" 
                         v-model="alunoNovo.contact"
-                        :rules="[value => !!value || 'O contato é obrigatorio']"></v-text-field>
+                        :rules="[value => !!value || 'O contato é obrigatorio']">
+                    </v-text-field>
                     </v-col>
 
                     <v-col cols="12" md="6">
                         <v-text-field 
                         type="date" 
                         label="Data de nascimiento" 
-                        v-model="alunoNovo.date_birth"></v-text-field>
+                        v-model="alunoNovo.date_birth">
+                    </v-text-field>
                     </v-col>
                 </v-row>
 
@@ -63,17 +64,18 @@
                         <v-text-field 
                         type="text" 
                         label="Cep" 
-                        v-model="alunoNovo.cep"
-                        :rules="[value => !!value || 'O CEP é obrigatorio']">  
+                        v-model="alunoNovo.cep" 
+                        @blur="fazerRequisicao"
+                        :rules="[value => !!value || 'O CEP é obrigatorio']">
                         </v-text-field>
                     </v-col>
 
                     <v-col cols="12" md="5">
                         <v-text-field 
                         type="text" 
-                        label="Endereco" 
+                        label="Logradouro" 
                         v-model="alunoNovo.street"
-                        :rules="[value => !!value || 'O endereco é obrigatorio']"> 
+                        :rules="[value => !!value || 'O endereco é obrigatorio']">
                         </v-text-field>
                     </v-col>
                     <v-col cols="12" md="2">
@@ -104,6 +106,7 @@
                         :rules="[value => !!value || 'O bairro é obrigatorio']">
                         </v-text-field>
                     </v-col>
+
                     <v-col cols="12" md="3">
                         <v-text-field 
                         type="text" 
@@ -117,23 +120,26 @@
                         <v-text-field 
                         type="text" 
                         label="Complemento" 
-                        v-model="alunoNovo.complement"></v-text-field>
+                        v-model="alunoNovo.complement">
+                        </v-text-field>
                     </v-col>
                 </v-row>
 
                 <v-row class="d-flex flex-row-reverse">
                     <v-col cols="12" md="2">
-                        <v-btn color="orange-darken-2" size="large" block  class="mt-2" type="submit">
+                        <v-btn 
+                        color="orange-darken-2" 
+                        size="large" 
+                        block 
+                        class="mt-2" 
+                        type="submit">
                             Cadastrar
                         </v-btn>
-
                     </v-col>
                 </v-row>
 
             </v-form>
         </v-card>
-
-
     </v-container>
 </template>
 
@@ -156,11 +162,32 @@ export default {
                 province: "",
                 complement: ""
             },
-            novoAlunoInfo: {},
-            
+            novoAlunoInfo: []
         }
     },
+
     methods: {
+
+        // Requisicao de dados da api CEP, com fetch
+
+        async fazerRequisicao() {
+            try {
+                const response = await fetch("https://viacep.com.br/ws/01001000/json/")
+                const data = await response.json()
+                this.alunoNovo.street = data.logradouro
+                this.alunoNovo.number = data.siafi
+                this.alunoNovo.province = data.uf
+                this.alunoNovo.neighborhood = data.bairro
+                this.alunoNovo.city = data.localidade
+                this.alunoNovo.complement = data.complemento
+
+            } catch (error) {
+                alert("Nao foi possivel concluir o cadastro")
+            }
+        },
+
+        // Validacao de dados do formulario
+
         async handleSubmit() {
             const { valid } = await this.$refs.form.validate()
 
@@ -170,7 +197,7 @@ export default {
             }
 
             try {
-                const result = await axios.post("http://localhost:3000/students",this.alunoNovo )
+                const result = await axios.post("http://localhost:3000/students", this.alunoNovo)
 
                 if (result.status === 200) {
                     localStorage.setItem("novoAluno_info", JSON.stringify(result.data))
@@ -184,19 +211,7 @@ export default {
         }
     },
 
-    mounted() {
-        axios.get("https://viacep.com.br/ws/01001000/json/")
-            .then((response) =>{
-                this.novoAlunoInfo = response.data               
-            })
-            .catch(() => {
-                alert("Nao foi possivel concluir o cadastro")
-            })
-
-    }
-
 }
-
 
 </script>
 
@@ -205,5 +220,4 @@ export default {
     border-bottom: solid;
     border-color: orange;
 }
-
 </style>
